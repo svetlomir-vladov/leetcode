03042d08ec6818485157b95728a15594d4aa27ff
Svetlomir Vladov <shecho1@proton.me>
2026-04-05 16:03:28 +0300

    finished 0202

--- solutions/cpp/easy/0202_happy_number.cpp
+++ solutions/cpp/easy/0202_happy_number.cpp
@@ -1,9 +1,12 @@
+#include <unordered_set>
 #include <iostream>
-
 class Solution {
 public:
     bool isHappy(int n) {
-        while (n > 2) {
+        std::unordered_set<int> seen_nums;
+
+        while (not seen_nums.count(n)) {
+            seen_nums.insert(n);
             int number = n;
             n = 0;
 
@@ -14,12 +17,12 @@ public:
             }
 
         }
-        return (n == 1) ? true : false;
+        return n == 1;
     }
 };
 
 int main() {
     Solution sol;
-    std::cout << sol.isHappy(3) << "\n";
+    std::cout << sol.isHappy(2) << "\n";
     return 0;
 }
